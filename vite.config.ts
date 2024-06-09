8d5ee0e2ff2ee094f79c9e7cf1b5edf685e7409d
MartinJaskulla <martinjaskulla@icloud.com>
2024-06-09 07:50:18 +0200

    Add QA Compass plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { qaCompassCodeJumpViteReact } from "@qa-compass/code-jump-vite-react";
+import { execSync } from "child_process";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({mode}) => {
+    const isDevelopment = mode === "development";
+    return {
+        plugins: [
+            ...(isDevelopment
+                ? [
+                    qaCompassCodeJumpViteReact({
+                        service: "github",
+                        repoUrl: execSync("git remote get-url origin")
+                            .toString()
+                            .trimEnd(),
+                        localRepoPath: process.env.PWD,
+                        branch: execSync("git rev-parse --abbrev-ref HEAD")
+                            .toString()
+                            .trimEnd(),
+                        commit: execSync("git rev-parse HEAD").toString().trimEnd(),
+                    }),
+                ]
+                : []),
+            react()
+        ],
+    }
 })
