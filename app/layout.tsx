47688b62669ba3d178060ac2de1cfea10e56048b
MartinJaskulla <martinjaskulla@icloud.com>
2024-06-12 21:22:15 +0200

    Add QA Compass script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script id="qa-compass">{`
+        {
+          const searchParams = new URLSearchParams(location.search)
+          if (searchParams.get('qaCompass') || searchParams.get('state')?.includes('qaCompass')) {
+            const script = document.createElement('script');
+            script.setAttribute('data-qa-compass-script-id', '79573e49-efd4-4cda-9da3-e20e9653a2e2')
+            script.src = 'https://www.unpkg.com/@qa-compass/toolbar/dist/index.js';
+            document.head.appendChild(script);
+          }
+        }
+      `}
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
   );
